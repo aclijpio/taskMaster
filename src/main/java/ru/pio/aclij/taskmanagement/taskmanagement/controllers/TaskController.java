48cdd7f26605e09ee745c9e9ec9efd8d5e3ea461
aclij <aclijpio@gmail.com>
2024-02-22 18:30:34 +0300

    SecurityConfig

--- src/main/java/ru/pio/aclij/taskmanagement/taskmanagement/controllers/TaskController.java
+++ src/main/java/ru/pio/aclij/taskmanagement/taskmanagement/controllers/TaskController.java
@@ -2,11 +2,9 @@ package ru.pio.aclij.taskmanagement.taskmanagement.controllers;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.pio.aclij.taskmanagement.taskmanagement.services.TaskService;
 import ru.pio.aclij.taskmanagement.taskmanagement.services.dtos.TaskDto;
 
@@ -18,12 +16,15 @@ import java.util.List;
 public class TaskController {
     private final TaskService service;
     @GetMapping
+    @ResponseStatus(HttpStatus.FOUND)
+
     private ResponseEntity<List<TaskDto>> getTasks(){
         return ResponseEntity.ok(
                 service.getAllTasks()
         );
     }
     @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.FOUND)
     private ResponseEntity<TaskDto> getTaskById(@PathVariable("id") int id){
         return ResponseEntity.ok(
                 service.getTasksById(id)
