f206f5ce3558e8d122af08bb5dd4e2371cf74fab
aclij <aclijpio@gmail.com>
2025-03-16 21:14:41 +0300

    Fix task service

--- src/main/java/ru/pio/aclij/taskmanagement/taskmanagement/controllers/ExceptionsController.java
+++ src/main/java/ru/pio/aclij/taskmanagement/taskmanagement/controllers/ExceptionsController.java
@@ -1,6 +1,5 @@
 package ru.pio.aclij.taskmanagement.taskmanagement.controllers;
 
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
